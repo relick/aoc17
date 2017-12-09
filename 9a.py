c848d2d0024212ea0c920968fb3ad53ca3ced722
Relick <t3h.twili@yahoo.co.uk>
2017-12-09 05:12:37 +0000

    9

---
+++ 9a.py
@@ -0,0 +1,29 @@
+def solution(lines):
+    score = 0
+    curgroup = 0
+    ingarbage = False
+    skip = False
+    for c in lines[0]:
+        if skip:
+            skip = False
+            continue
+        elif c == "!":
+            skip = True
+            continue
+        elif c == "{" and not ingarbage:
+            curgroup += 1
+        elif c == "}" and not ingarbage:
+            score += curgroup
+            curgroup -= 1
+        elif c == "<" and not ingarbage:
+            ingarbage = True
+        elif c == ">" and ingarbage:
+            ingarbage = False
+    print(score)
+
+
+if __name__ == "__main__":
+    import sys
+    tfile = open(sys.argv[1], 'r')
+    solution(tfile.readlines())
+    #solution(sys.argv[1])
