fdeeed99b1c74e03733ddd3e6df02c3ab52916e5
Relick <t3h.twili@yahoo.co.uk>
2017-12-11 05:31:33 +0000

    11

---
+++ 11a.py
@@ -0,0 +1,46 @@
+def getdist(x, y):
+    steps = 0
+    while x != 0:
+        steps += 1
+        if x > 0:
+            x -= 1
+            if y > 0:
+                y -= 0.5
+            else:
+                y += 0.5
+        else:
+            x += 1
+            if y > 0:
+                y -= 0.5
+            else:
+                y += 0.5
+    steps += abs(y)
+    return steps
+def solution(lines):
+    x = 0
+    y = 0
+    for s in lines[0].split()[0].split(','):
+        if s == 'n':
+            y += 1
+        elif s == 'ne':
+            y += 0.5
+            x += 1
+        elif s == 'se':
+            y -= 0.5
+            x += 1
+        elif s == 's':
+            y -= 1
+        elif s == 'sw':
+            y -= 0.5
+            x -= 1
+        elif s == 'nw':
+            y += 0.5
+            x -= 1
+    print(getdist(x,y))
+        
+
+if __name__ == "__main__":
+    import sys
+    tfile = open(sys.argv[1], 'r')
+    solution(tfile.readlines())
+    #solution(sys.argv[1])
